3dc6acf21854ae1a8cb9da54534a5c55d3c5d518
srinu217 <gokarla_fw11_270@masai.school>
2021-11-07 17:48:27 +0530

    updated

---
+++ sprint-4/coding/tailwand/tailwind.config.js
@@ -0,0 +1,21 @@
+
+
+module.exports = {
+
+theme : {
+    colors : {
+        aabbcc: '#b1b1b1',
+    },
+},
+
+
+extend: {
+    spacing: {
+      '50': '100px',
+      '144': '36rem',
+    },
+    borderRadius: {
+      '4xl': '2rem',
+    }
+  }
+};
