fe0fed8b4530569d09100cba19536c6ea33f98db
srinu217 <gokarla_fw11_270@masai.school>
2021-11-07 17:33:10 +0530

    s

---
+++ sprint-3/coding/foodapp/components/navbar1.js
@@ -0,0 +1,17 @@
+function navbar1(){
+    return ` 
+
+
+    <div id="options">
+     <img src="https://www.themealdb.com/images/logo-small.png"/>
+     <button id="home">Home</button>
+     <h3><a href="#">Jewellery</a>API</h3>
+     <h3><a href="#">Jewellery</a>Forum</h3>
+     <a href="https://facebook.com"><ion-icon name="logo-facebook></ion-icon></a>
+     <a href="https://twitter.com"><ion-icon name="logo-twitter></ion-icon></a>
+     <input id="name" type="text" placeholder="seaerch meals">
+  
+  </div>`
+}
+
+export default navbar1
